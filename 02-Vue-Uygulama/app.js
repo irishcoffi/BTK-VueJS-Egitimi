1c810346053bac75ec5fa8d9707dc854752cf33e
irishcoffi <irfanyuksell@outlook.com>
2024-05-23 16:10:24 +0300

    Update app.js

--- 02-Vue-Uygulama/app.js
+++ 02-Vue-Uygulama/app.js
@@ -3,7 +3,7 @@ const app = Vue.createApp({
   <h2>Başlık: {{baslik}}</h2>
   <h3>İçerik: {{icerik}}</h3>
   <p>Yorum Sayısı: {{yorumSayisi}}</p>
-  <button v-on:click="ekle()">Ekle</button>
+  <button v-on:click="ekle()">Ekle</button>        //ekle methodunu çalıştırır console logu ekler.
   <p>{{yazi}}</p>
   `,
   data() {
@@ -14,10 +14,10 @@ const app = Vue.createApp({
       yazi:'Yorum eklemek için tıklayın.'
     }
   },
-  methods: {
+  methods: {            //datanın methodlarını eklemek için özelliklerini belritmek için 2. butonla ile bağlı
     ekle(){
       console.log('ekle metodu çalıştı');
-      this.yorumSayisi++;
+      this.yorumSayisi++;          //yorum sayısı bir üstte olduğu için this kullanılır.
       this.yazi=`${this.yorumSayisi} tane yorum girildi.`
     }
   },
